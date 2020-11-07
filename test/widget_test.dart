fa677cac8134eb8435074936e8318e8bd89d9530
naman kumar <kumarnaman653@gmail.com>
2020-11-07 12:32:43 +0530

    enter

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:rwa_app/main.dart';
+import 'package:RWA/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
