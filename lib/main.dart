ac9be57bf51d725f3ed83bc74d5e9d79bd9a3d11
Sumit Garg <gargs473@gmail.com>
2020-11-09 14:47:50 +0530

    example_app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,36 @@
+import 'package:RWA/add_member_screen.dart';
+import 'package:RWA/global_buzz.dart';
+import 'package:RWA/login_or_register_screen.dart';
+import 'package:RWA/profile.dart';
 import 'package:flutter/material.dart';
-import 'profile.dart';
+import 'login_screen.dart';
 
 void main() => runApp(MyApp());
 
+// ignore: camel_case_types
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Profile();
+    return MaterialApp(
+      theme: ThemeData(
+        inputDecorationTheme: InputDecorationTheme(
+          focusedBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.blueAccent,
+            ),
+          ),
+          enabledBorder: UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Color(0xFFACADB1),
+            ),
+          ),
+        ),
+      ),
+      initialRoute: LoginOrRegisterScreen.id,
+      routes: {
+        LoginOrRegisterScreen.id: (context) => LoginOrRegisterScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+      },
+    );
   }
 }
